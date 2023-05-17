8630d38f3fe6f5c59c7f1e6d2191245e66e27195
tenderne <82151982+Tenderne-code@users.noreply.github.com>
2023-05-17 12:36:49 +0800

    code

---
+++ lab0-introduction-Tenderne-code/build/CMakeFiles/hellonetwork.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellonetwork.dir/hellonetwork.cpp.o"
+  "hellonetwork"
+  "hellonetwork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellonetwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
